97dffdb0a18a448277c7613df19bb4ef37097198
Tihomir Tinkov <tihomir.y.t.m6@gmail.com>
2026-07-23 17:00:50 +0300

    Add .env loading and move docs

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,12 +3,17 @@ package main
 import (
 	"github.com/Tihomir-Tinkov/cooking-site-project/internal/config"
 	"github.com/Tihomir-Tinkov/cooking-site-project/internal/logger"
+	"github.com/joho/godotenv"
 	"github.com/rs/zerolog/log"
 
 	appbuilder "github.com/Tihomir-Tinkov/cooking-site-project/internal/app"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Error().Err(err).Msg("failed to load .env file, using system environment")
+	}
+
 	conf := config.Config{}
 
 	if err := conf.Parse(); err != nil {
